955bcd5e8abdd27c5fd94a9399c4ff16ead0b7b4
renqingjie@pku.edu.cn <renqingjie@pku.edu.cn>
2017-03-12 15:11:59 +0800

    a little change

---
+++ Python/0312/01.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Jack
+# @Date:   2017-03-12 15:00:54
+# @Last Modified by:   Jack
+# @Last Modified time: 2017-03-12 15:09:53
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+# %matplotlib inline
+from matplotlib.pylab import rcParams
+rcParams['figure.figsize']=15,6
+
+#import the data
+data = pd.read_csv("AirPassengers.csv")
+print(data.head())
+print("\n Data Types:")
+print(data.dtypes)
+
+
+dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m')
+data = pd.read_csv("AirPassengers.csv",parse_dates="Month", index_col="Month", date_parser=dateparse)
+print(data.head())

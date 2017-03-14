d99db4ca3e173a9f1fcb8ca5302f93a4c0ea904c
renqingjie@pku.edu.cn <renqingjie@pku.edu.cn>
2017-03-14 23:07:50 +0800

    a little work

--- R/0313/02.R
+++ R/0313/02.R
@@ -7,6 +7,11 @@ wilcox.test(x1, x2)
 wilcox.test(x1, x2, paired=TRUE)
 
 ## chi-square test
+x <- c(18, 13, 17, 21, 15, 16)
+p <- rep(1/6, 6)
+chisq.test(x, p)
+chisq.test(x)
 
-
+tab <- matrix(c(60, 3, 32, 11),nrow=2, ncol=2)
+chisq.test(test)
 

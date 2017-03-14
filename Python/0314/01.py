1cc9bdb18c083515fc89d1cf50e7cb1dae480273
renqingjie@pku.edu.cn <renqingjie@pku.edu.cn>
2017-03-14 23:22:42 +0800

    a little work undone

--- Python/0314/01.py
+++ Python/0314/01.py
@@ -3,5 +3,14 @@
 #做计时实验，比较List和Dict的del操作符性能
 #给定一个随机顺序的数列表，写一个复杂度为O(nlogn)的求第k小的数的算法
 #请改进上述的算法，使之复杂度降低为O(n)
-
 import timeit
+import random
+
+# Question 1
+t1 = timeit.Timer("k=x[i]", "from __main__ import x")
+t2 = timeit.Timer("k=i","from __main__ import x")
+
+for i in range(1,10000):
+    k = random.randrange(%d) %i
+    print("")
+

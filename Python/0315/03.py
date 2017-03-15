8964a6939a1b1ca5736e6475ecbf37640854124f
renqingjie@pku.edu.cn <renqingjie@pku.edu.cn>
2017-03-15 23:32:23 +0800

    a little work 3rd time

--- Python/0315/03.py
+++ Python/0315/03.py
@@ -21,3 +21,19 @@ import random
 #     print("%d,%10.5f, %10.5f" % (i, getItemTime, setItemTime))
 
 #做计时实验，比较List和Dict的del操作符性能
+# print("i List Dict")
+# for i in range(1, 100001, 1000):
+#     lis = list(range(i))
+#     t4 = timeit.Timer("del lis[random.randrange(%d)]" % i,
+#                       "from __main__ import lis, random")
+#     delListTime = t4.timeit(number=1)
+
+#     dic = {j: None for j in range(i)}
+#     t5 = timeit.Timer("del dic[random.randrange(%d)] " % i,
+#                       "from __main__ import dic, random")
+#     delDictTime = t5.timeit(number=1)
+
+#     print("%d, %10.5f, %10.5f" % (i, delListTime*10000, delListTime*10000))
+
+
+

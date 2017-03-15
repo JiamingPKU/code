631cdfda7aa07df1a700287bdcff9259d1eec6b9
renqingjie@pku.edu.cn <renqingjie@pku.edu.cn>
2017-03-15 17:02:17 +0800

    a little work *2

--- Python/0315/03.py
+++ Python/0315/03.py
@@ -2,9 +2,22 @@
 import timeit
 import random
 
-for i in range(1, 100001, 1000):
-    x = list(range(i))
-    t = timeit.Timer("k = x[random.randrange(%d)]" % i,
-                     "from __main__ import x, random")
-    lst_time = t.timeit(number=1000)
-    print(i,lst_time)
+# for i in range(1, 100001, 1000):
+#     x = list(range(i))
+#     t1 = timeit.Timer("k = x[random.randrange(%d)]" % i,
+#                      "from __main__ import x, random")
+#     lst_time = t1.timeit(number=1000)
+#     print(i,lst_time)
+
+#做计时实验，验证Dict的get item和set item都是O(1)的
+# for i in range(1, 100001, 1000):
+#     dic = {j: None for j in range(i)}
+#     t2 = timeit.Timer("dic[random.randrange(%d)] = 1" % i,
+#                       "from __main__ import dic, random")
+#     getItemTime = t2.timeit(number=1000)
+#     t3 = timeit.Timer("k = dic[random.randrange(%d)]" % i,
+#                       "from __main__ import dic, random")
+#     setItemTime = t3.timeit(number=1000)
+#     print("%d,%10.5f, %10.5f" % (i, getItemTime, setItemTime))
+
+#做计时实验，比较List和Dict的del操作符性能

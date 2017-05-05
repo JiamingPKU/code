bfd3a4b67e2d3024fe5c9e0141890262f0840da9
LENOVO-PC\Ren Qingjie <renqingjie@pku.edu.cn>
2017-05-05 12:29:20 +0800

    no message

--- R/0504/比较税收作业.R
+++ R/0504/比较税收作业.R
@@ -41,25 +41,24 @@ ggplot(m1data, aes(x=incomeTax))+geom_histogram()
 ggplot(m1data, aes(x=revenueTax))+geom_histogram()
 ggplot(m2data, aes(x=dif))+geom_histogram()
 
+#处理时间
+names(mdata)
+mdata$year <- as.Date(mdata$Accper,"%Y/%m/%d")
+mdata$year <- format(mdata$year, "%y")
 
-##处理时间
-far$year = as.Date(far$Accper,"%Y/%m/%d")
-far$year = format(year, format="%y")
-table(far$year)
 
-##处理变量名称
-library(reshape)
-far1 <- rename(far, c(A100000="asset", B110101="income", D100000="cashflow", T30100="alratio"))
-names(far1)
-
-#install.packages("ggplot2")
 library(ggplot2)
-ggplot(data=far1, aes(x=Etaxrt)) + geom_histogram()
+ggplot(data=far, aes(x=Etaxrt)) + geom_histogram()
 #发现有许多离群值，去除离群值；两端各去除0.5%的异常值
-quantile(far1$Etaxrt, probs = c(0.005,0.995), na.rm=TRUE)
+quantile(far$Etaxrt, probs = c(0.005,0.995), na.rm=TRUE)
+far1 <- far
+far1$year <- as.Date(far1$Accper, "%Y/%m/%d")
+far1$year <- format(far1$year, "%y")
 far1$Etaxrt <- winsor(far1$Etaxrt, fraction = 0.005)
-ggplot(data=far1, aes(x=Etaxrt)) + geom_histogram()
-
+ggplot(data=far1, aes(x=Etaxrt)) + geom_histogram()+
+  labs(x="实际税率",y="数量")
+ggplot(data=far1, aes(x=Etaxrt, fill=year)) + geom_density()+
+  labs(x="实际税率",y="数量")+facet_grid(~year~.)
 
 
 mdata=merge(alltax, othertax, by=intersect(c("Stkcd","Accper"), c("Stkcd","Accper")) 

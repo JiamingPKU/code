c261aed102f4bbd830029d63cdc3abf1d1e9a8c7
LENOVO-PC\Ren Qingjie <renqingjie@pku.edu.cn>
2017-05-23 16:15:48 +0800

    anaconda test

---
+++ Python/test/dataNitro/test1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: Ren Qingjie
+# @Date:   2017-05-23 14:02:35
+# @Last Modified by:   Ren Qingjie
+# @Last Modified time: 2017-05-23 14:06:23
+
+
+import math
+
+
+Cell(1,1) = math.pi
+
